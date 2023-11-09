bfe4248e9fad7fcb79c8475fa7608a391c5e94a8
asmita780 <asmita22@navgurukul.org>
2023-11-09 09:45:27 +0530

    complet

--- CreatingandaappendingHTML.js
+++ CreatingandaappendingHTML.js
@@ -1,4 +1,4 @@
-// textContent propety => for adding text in <p>
+// textContent propety => for adding text in <p> 
 
 // const newPara = document.createElement("p");
 // newPara.textContent = "This is a paragraph";
@@ -17,12 +17,18 @@
 
 // inserting HTML
 // insertAdjacentHTML => convert string to HTML =>iner this mathod there is a parsar who convert string to an HTML 
-const divbox = `
-<div class="box" id="box-5">box-5</div>
-<div class="box" id="box-6">box-6</div>
-`;
+// const divbox = 
+// `<div class="box" id="box-5">box-5</div>
+// <div class="box" id="box-6">box-6</div>
+// `;
 
 // container.insertAdjacentHTML("beforeend", divbox);//"beforeend" => it's a posstion where we want to put our new elements(divbox)
 // container.insertAdjacentHTML("beforebegin", divbox);
 // container.insertAdjacentHTML("afterend", divbox);
 // container.insertAdjacentHTML("afterbegin", divbox);
+
+
+
+
+
+// lex besic 50%
